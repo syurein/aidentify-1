23c632e0dcf7a19dcc7e6ec628fd4e94f84c7150
Open Kitchens <123849081+OpenKitchens@users.noreply.github.com>
2024-10-19 21:21:16 +0900

    gg

--- src/components/upload.vue
+++ src/components/upload.vue
@@ -37,23 +37,46 @@ const upload = (event) => {
   };
   reader.readAsDataURL(document.getElementById('uploadImage').files[0]);
 };
+
+const dialog = ref(false)
 </script>
 
 <template>
   <div class="relative block bg-[#90838059] w-[90vw] rounded-xl backdrop-blur-sm">
     <button class="position absolute right-0 invert"><img src="@/assets/images/x.svg" class="w-6 h-6 mr-3 mt-3"
-        @click="store.router('title')"></button>
+        @click="store.router('title'); dialog = false"></button>
     <h2 class="text-3xl font-bold text-center text-white p-7">画像をアップロード</h2>
-    <div><button @click="openFileDialog"
+    <div><button @click="dialog = true"
         class="relative left-[50%] translate-x-[-50%] rounded-xl bg-indigo-400 text-xl px-5 py-4 font-bold bg-indigo-500 text-white w-[80vw]">Upload
         image</button>
     </div>
-    <div class="mt-4">
-      <button @click="store.router('take')"
-        class="relative left-[50%] translate-x-[-50%] rounded-xl bg-indigo-400 text-xl px-5 py-4 font-bold bg-indigo-500 text-white w-[80vw]">Take
-        photo</button>
-    </div>
+    <Transition>
+      <div class="mt-4 flex flex-wrap w-48 shadow-lg shadow-black-500/50 ml-5" v-show="dialog">
+        <button @click="openFileDialog"
+          class="rounded-t-[0.6rem] bg-[#ffffffaa] text-sm px-3 py-2 text-black w-48 hover:bg-[#ffffff99] text-left">写真ライブラリ
+          <img src="@/assets/images/picture.svg" class="ml-[45px] inline-block invert w-5"></button>
+        <button @click="store.router('take')"
+          class="bg-[#ffffffaa] text-sm px-3 py-2 text-black w-48 mt-[1px] hover:bg-[#ffffff7e] text-left">写真を撮る <img
+            src="@/assets/images/camera-b.svg" class="ml-[73px] inline-block invert w-5"></button>
+        <button @click="openFileDialog"
+          class="rounded-b-[0.6rem] bg-[#ffffffaa] text-sm px-3 py-2 text-black mt-[1px] w-48 hover:bg-[#ffffff7e] text-left">ファイルを選択
+          <img src="@/assets/images/folder.svg" class="ml-[45px] inline-block invert w-5"></button>
+      </div>
+    </Transition>
     <input type="file" ref="fileInput" @change="upload" id="uploadImage" accept="image/*" hidden>
     <div class="h-40"></div>
   </div>
 </template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease-out;
+}
+
+.v-enter-from,
+.v-leave-to {
+  transform: translateY(50px);
+  opacity: 0;
+}
+</style>
