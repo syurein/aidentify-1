c9c7aac142306d2012dd4283b1d547e019e68004
syurein <127266308+syurein@users.noreply.github.com>
2025-01-10 11:14:07 +0900

    Update url.js

--- src/stores/url.js
+++ src/stores/url.js
@@ -8,15 +8,15 @@ export const useUrlStore = defineStore('url', () => {
 
   const type = {
     "w": "https://rein0421-aidentify.hf.space",
-    "c": "https://cool-emu-learning.ngrok-free.app",
-    "p": "https://powerful-centrally-chimp.ngrok-free.app"
+    "c": "https://rein0421-aidentify.hf.space",
+    "p": "https://rein0421-aidentify.hf.space"
   }
 
   const change = (changeType) => {
     data.value = type[changeType]
   }
 
-  const data = ref("https://wired-kitten-adequately.ngrok-free.app")
+  const data = ref("https://rein0421-aidentify.hf.space")
 
   return { data, change }
 })
